44c9a1e45dff0f1e1e2c257fe94198b8bb540094
Junesoo Shin <junesooshin@gmail.com>
2019-01-13 19:52:03 +0100

    Changes to main

--- main.py
+++ main.py
@@ -6,23 +6,36 @@ redLed=machine.Pin(15,machine.Pin.OUT)
 blueLed=machine.Pin(27,machine.Pin.OUT)
 greenLed=machine.Pin(33,machine.Pin.OUT)
 button1=machine.Pin(36,machine.Pin.IN,machine.Pin.PULL_UP)
-button2=machine.Pin(4,machine.Pin.IN,machine.Pin.PULL_UP)
-dataIn=machine.ADC(machine.Pin(34))
-dataIn.atten(ADC.ATTN_11DB)
+button2=machine.Pin(39,machine.Pin.IN,machine.Pin.PULL_UP)
+file=open('datafile.txt','w')
+
+#Turn on only blue LED
+redLed.value(1)
+greenLed.value(1)
+
+data=machine.ADC(machine.Pin(32))
+data.atten(ADC.ATTN_11DB)
+#data.width(ADC.WIDTH_11BIT)
 
 mainLed=machine.PWM(machine.Pin(12))
 mainLed.duty(100)
 mainLed.freq(78000)
-dataIn.atten(ADC.ATTN_11DB)
-
 
 while True:
-	while not button1.value()==0:
-		time.sleep(0.01)
-	print("hello")
-	first=button1.value()
-	time.sleep(0.01)
-	second=button1.value()
-	if first and not second:
-		print("hello")
-		break
+    if button1.value()!=0:
+        blueLed.value(1)
+        redLed.value(0)
+
+        while True:
+            if button2.value()!=0:
+                redLed.value(1)
+                greenLed.value(0)
+                break
+            else:
+                #need to do a conversion before recording?
+                file.write(repr(data.read())+';')
+                time.sleep(10)
+        break
+
+file.close()
+
