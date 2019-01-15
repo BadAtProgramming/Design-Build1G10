9922bb42414e39560e814e0e4e3ec533bb91d328
JK <josie@amafun.co.za>
2019-01-15 09:43:01 +0100

    final plots

--- DataPlot.py
+++ DataPlot.py
@@ -11,18 +11,25 @@ import matplotlib.pyplot as plt
 
 def DataPlot():
     filename=input("Name of file you want to plot: ")
-    data=pd.read_csv(filename,sep=";",header=None)
+    Ref=float(input("Measured Reference Light Intensity: "))
+    #change back to ';' 
+    data=pd.read_csv(filename,sep=",",header=None)
     DM=np.asmatrix(data)
-    x=np.array(DM[:,1])
     y=np.array(DM[:,0])
-    plt.plot(x, y, color='red')
-    plt.xlabel('PWM Rate (LED)')
-    plt.ylabel('ADC (Light Sensor)')
-    plt.title('Yeast Cell Growth')
+    l=len(y)
+    x=np.arange(0,l,1)
+    y=np.transpose(y)
+    OD=np.zeros(np.size(y))
+    for id in y:
+        OD=-np.log10(id/Ref)
+        #Can also be written as np.log10(Ref/id)
+    plt.plot(x, OD, color='red',markersize=0.5)
+    plt.xlabel('Time (min)')
+    plt.ylabel('OD')
+    plt.title('Yeast Cell Growth (OD vs. Time)')
     #plt.xticks(range(1,x+1))
     #plt.yticks(range(1,y+1))
     plt.show()
-    print(data)
 DataPlot()    
     
     
