3450b75a913200255ad1ae12e5544497ac768ec1
Unknown <johan@levinjensen.dk>
2019-01-15 14:21:39 +0100

    Main script first run

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import machine
 import time
 from machine import ADC
 import utime
+import math
 
 redLed=machine.Pin(15,machine.Pin.OUT)
 blueLed=machine.Pin(27,machine.Pin.OUT)
@@ -22,8 +23,6 @@ mainLed.freq(78000)
 mainLed.duty(600)
 
 timer=0
-k=0
-r=0
 
 while True:
     if button1.value()!=0:
@@ -34,20 +33,25 @@ while True:
             if button2.value()!=0:
                 redLed.value(0)
                 greenLed.value(1)
-                while k<100:
-                    r+=data.read
-                    k+=1
-                r=r/100
-                file.write(repr(r)))
                 break
 
             elif timer==6000: #for sleep(0.01), 1 second interval -> timer=100
+                k=0
+                r=[]
+                acu=0
+                cal=0
                 while k<100:
-                    r+=data.read
+                    d=data.read()
+                    r.append(d)
+                    acu+=d
                     k+=1
                     time.sleep(0.001)
-                r=r/100
-                file.write(repr(r)+ ';'))
+                acu=acu/100
+                for el in r:
+                    cal+=(float(el)-acu)**2
+                cal=math.sqrt(cal/100)
+                file.write(repr(acu) + ',' + repr(cal) + ';')
+                r.clear()
                 timer=10
             else:
                 timer+=1
